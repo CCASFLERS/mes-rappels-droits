660835686d410e753993fc3463c24cde297d7d18
CCASFLERS <martingoup@gmail.com>
2026-05-05 14:30:41 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mes-rappels-droits-v8';
+const CACHE_NAME = 'mes-rappels-droits-v9';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
